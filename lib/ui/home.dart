7c8a36a264d8ea3b7c165b58e5452bc9726c3242
robin3317 <abdurrahmanrobin47@gmail.com>
2018-07-14 20:10:37 +0600

    primary setup for development

---
+++ lib/ui/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new HomeState();
+  }
+
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold();
+  }
+}
