74cfe82f4975926728118717f6c77335e69427b4
robin3317 <abdurrahmanrobin47@gmail.com>
2018-07-14 21:10:50 +0600

    image is added

--- lib/main.dart
+++ lib/main.dart
@@ -4,4 +4,5 @@ import './ui/home.dart';
 void main() => runApp(new MaterialApp(
   title: "Currency Converter",
   home: new Home(),
+  debugShowCheckedModeBanner: false,
 ));
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -5,12 +5,38 @@ class Home extends StatefulWidget {
   State<StatefulWidget> createState() {
     return new HomeState();
   }
-
 }
 
 class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold();
+    return new Scaffold(
+      //appbar
+      appBar: new AppBar(
+        title: new Text("Currency Converter",
+          style: new TextStyle(fontSize: 25.0)
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.lightGreen,
+      ),
+      backgroundColor: Colors.blueGrey,
+
+      //body
+      body: new Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          new Padding(padding: EdgeInsets.only(top: 20.0)),
+          //image
+          new Center(
+            child: new Image.asset("images/currency.png", width: 150.0, height: 100.0),
+          ),
+        ],
+
+      ),
+        //text field
+        //radio button
+        //convert button
+        //show result
+    );
   }
 }
